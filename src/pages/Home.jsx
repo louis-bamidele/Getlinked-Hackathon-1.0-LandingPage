ea0ba32a698bc527c626bddde94c6a05977eef3e
Martinsadepoju4 <martinsadepoju4@gmail.com>
2023-09-22 04:10:58 +0100

    completed hero media queries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,43 +10,48 @@ const Home = () => {
   return (
     <div className="App">
       <Nav />
-      <div className="text-white pl-20">
-        <h2 className="pt-6 text-4xl pr-14 text-left">
+      <div className="font-bold mx-auto text-white lg:pl-20 lg:mx:0 lg:w-auto">
+        <h2 className="text-[16px] text-center lg:w-auto lg:pt-4 lg:text-[25px] lg:pr-14 lg:text-right hero-h2">
           igniting a Revolution in HR Innovation
         </h2>
-        <div className="flex justify-between pr-14">
-          <img src={star} alt="star" />
+        <div className="pr:0 w-[90vw] m-auto lg:w-auto flex justify-between lg:pr-14 ">
+          <img className="star" src={star} alt="star" />
           <img src={vector4} alt="curve line"></img>
         </div>
-        <div className="flex pt-4">
-          <div className="w-6/12">
-            <div>
-              <h1 className="text-80px leading-normal">
-                getlinked Tech Hackathon <span>1.0</span>{" "}
-                <img className="inline" src={chain} alt="chain" />
+        <div className="flex flex-col lg:flex-row">
+          <div className="w-[85vw] lg:w-auto m-auto lg:m-0 text-center lg:text-left">
+            <div className="relative">
+              <h1 className="mt-6 font-bold relative text-[2.5rem] lg:text-[3.7rem] leading-none hero-h1">
+                getlinked Te<span className="hero-c-text">c</span>h <br />{" "}
+                Hackathon <span className="hero-span">1.0</span>{" "}
               </h1>
-              <img src={creative1} alt="creative" />
             </div>
 
-            <p>
+            <p className="text-[13px lg:text-sm">
               Participate in getlinked tech Hackathon 2023 stand a chance to win
               a Big prize
             </p>
-            <button>Register</button>
-            <div className="flex">
+            <button className="nav-text btn my-8 w-min  cursor-pointer">
+              Register
+            </button>
+            <div className="text-center m-auto lg:m-0 flex justify-between text-[48px] w-[214px] h-[64px] lg:text-[2rem] lg:w-[200px] lg:h-[85px]">
               <h1>
-                00<span>H</span>
+                00<span className="text-sm">H</span>
               </h1>
               <h1>
-                00<span>M</span>
+                00<span className="text-sm">M</span>
               </h1>
               <h1>
-                00<span>S</span>
+                00<span className="text-sm">S</span>
               </h1>
             </div>
           </div>
-          <div className="hero-man">
-            <img src={heroGlobeImage} alt="hero" />
+          <div className="w-[95vw] m-auto lg:m-0 hero-man lg:pr-2.5">
+            <img
+              className="w-[100%] h-[auto] lg:m-auto lg:w-[65%] lg:h-[90%]"
+              src={heroGlobeImage}
+              alt="hero"
+            />
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    fontSize: {
-      "80px": "80px",
-    },
-    // extend: {},
+    // fontSize: {
+    //   "80px": "80px",
+    // },
+    extend: {},
   },
   plugins: [
     require("@tailwindcss/forms")({
